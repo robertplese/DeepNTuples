da41b3ee71f27e0fc0cadf72f04c1da100bee838
robertplese <148449471+robertplese@users.noreply.github.com>
2025-08-21 10:03:49 +0200

    Update TrackInfoBuilder.h

--- DeepNtuplizer/interface/TrackInfoBuilder.h
+++ DeepNtuplizer/interface/TrackInfoBuilder.h
@@ -102,12 +102,11 @@ public:
 
 	    // Loop over tracks_ (generalTracks from RECO level) to find the one that matches the bestTrack (assigned to PackedCandidate from MiniAOD level)
     		for (size_t i = 0; i < tracks_->size(); ++i) {
-        		if ((std::abs((*tracks_)[i].pt()-bestTr->pt())<0.01) && 
-            	 	(std::abs((*tracks_)[i].eta()-bestTr->eta())<0.01) && 
-            	 	(std::abs((*tracks_)[i].phi()-bestTr->phi())<0.01)) {
-            			matchedTrackRef = reco::TrackRef(tracks_, i);  // build TrackRef from collection and index
-            			break;
-        		}
+        		if ((std::abs((*tracks_)[i].pt()-bestTr->pt())<0.01) &&
+                (reco::deltaR((*tracks_)[i].eta(),(*tracks_)[i].phi(),bestTr->eta(),bestTr->phi()))<0.01) {
+                    	matchedTrackRef = reco::TrackRef(tracks_, i);  
+                        break;
+                    }
     		}
 
     		if (matchedTrackRef.isNonnull() && matchedTrackRef.isAvailable()) {
